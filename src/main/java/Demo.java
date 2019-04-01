fcb0b45dc406f4702348d99e81645f84fcdce0ca
zhaohuisheng <505002774@qq.com>
2019-04-01 21:06:30 +0800

    第一次上传

---
+++ src/main/java/Demo.java
@@ -0,0 +1,5 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("这是第一个上传的Demo");
+    }
+}
---
+++ src/main/java/Demo14.java
@@ -0,0 +1,6 @@
+public class Demo14 {
+    public static void main(String[] args) {
+        System.out.println("在上传一个Demo1");
+        System.out.println("我需要修改Demo14这个文件");
+    }
+}
